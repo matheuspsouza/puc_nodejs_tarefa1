9f73c42903b49bf37b1dc040accc35fcccaa3d49
matheuspsouza <matheuspatrocinio@hotmail.com>
2022-02-26 00:11:34 -0300

    mudando para express

--- app0.js
+++ app0.js
@@ -1,28 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 5000;
+// const hostname = '127.0.0.1';
+// const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('MATHEUS PATROCINIO SOUZA');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('MATHEUS PATROCINIO SOUZA');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.get('/', (req,res)=>{
-//     res.send('MATHEUS PATROCINIO SOUZA');
-// })
+app.get('/', (req,res)=>{
+    res.send('MATHEUS PATROCINIO SOUZA');
+})
 
-// const port  = process.env.PORT || '5000';
+const port  = process.env.PORT || '5000';
 
-// app.listen(port, () => console.log(`Server started on PORT ${port}`));
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
