805b2b9165bc047e60ce3a51b83f6cef05ec02b7
matheuspsouza <matheuspatrocinio@hotmail.com>
2022-02-26 00:08:26 -0300

    add files

--- app0.js
+++ app0.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +12,17 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.get('/', (req,res)=>{
+//     res.send('MATHEUS PATROCINIO SOUZA');
+// })
+
+// const port  = process.env.PORT || '5000';
+
+// app.listen(port, () => console.log(`Server started on PORT ${port}`));
